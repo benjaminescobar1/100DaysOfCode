0176f86d11b06f8e26f5146c88714a26f461941d
Benjamin Escobar <benjaminescobar@Benjamins-MacBook-Pro.local>
2026-01-04 19:02:38 -0800

    Adding Day1 Band Name Generator

---
+++ Day1-BandNameGenerator/main.py
@@ -0,0 +1,12 @@
+# Welcome User
+print("Welcome to the Band Name Generator.")
+
+# Input City
+city = input("What's the name of the city you grew up in?\n")
+
+# Input Pet Name
+pet = input("What's your pet's name?\n")
+
+# Print Band Name Generator
+print("Your band name could be: " + city + " " + pet)
+
